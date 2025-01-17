c56cf602089fb0d816afb1de7d31978dd6432593
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2025-01-17 11:18:11 +0100

    Displaying a Location Preview Map Snapshot via Google.

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -18,6 +18,16 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=***';
+  }
+
   void _getCurrentLocation() async {
     Location location = Location();
 
@@ -76,6 +86,15 @@ class _LocationInputState extends State<LocationInput> {
             color: Theme.of(context).colorScheme.onBackground,
           ),
     );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
