7ad86a9114220968db46e8f586425920e6e60e12
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2025-01-17 10:09:51 +0100

    Adding the "location" Package & Starting with the "Get Location" Input Widget

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -5,7 +5,7 @@ import 'package:image_picker/image_picker.dart';
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key, required this.onPickImage});
 
-  final void Function (File image) onPickImage;
+  final void Function(File image) onPickImage;
 
   @override
   State<StatefulWidget> createState() {
@@ -55,10 +55,11 @@ class _ImageInputState extends State<ImageInput> {
 
     return Container(
       decoration: BoxDecoration(
-          border: Border.all(
-        width: 1,
-        color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-      )),
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+        ),
+      ),
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
